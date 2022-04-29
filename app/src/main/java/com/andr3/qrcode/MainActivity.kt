3cb95bae83b211236fb41ff961c1fced95d831c2
ANDR3LU1Z-dot <andre.3117834@gmail.com>
2022-04-29 18:13:19 -0400

    Configurando o qrcode na MainActivity.kt com o EasyPermissions

--- app/src/main/java/com/andr3/qrcode/MainActivity.kt
+++ app/src/main/java/com/andr3/qrcode/MainActivity.kt
@@ -1,11 +1,102 @@
 package com.andr3.qrcode
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.andr3.qrcode.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import com.journeyapps.barcodescanner.CaptureActivity
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks{
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val button: Button = binding.btnQrCode
+
+        button.setOnClickListener{
+
+        }
     }
+
+    private fun hasCameraAcess(): Boolean{
+        return EasyPermissions.hasPermissions(this, android.Manifest.permission.CAMERA)
+    }
+
+    private fun cameraTask(){
+
+        if(hasCameraAcess()){
+            var qrScanner = IntentIntegrator(this)
+            qrScanner.setPrompt("scan a QR code")
+            qrScanner.setCameraId(0)
+            qrScanner.setOrientationLocked(true)
+            qrScanner.setBeepEnabled(true)
+            qrScanner.captureActivity = CaptureActivity::class.java
+            qrScanner.initiateScan()
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs access to your camera so you can take pictures",
+                123,
+                android.Manifest.permission.CAMERA
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK){
+            val result: IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            val qrcodeValor = result.contents
+
+
+
+        }
+
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this@MainActivity, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
